6bd566cbb00f97dc1ecf3d14e4513ca27b2dc8c2
李杰 <myjobemail1@163.com>
2018-07-10 14:43:35 +0800

    提交mybatis为主版本

---
+++ albedo-boot-api/albedo-boot-service/src/main/java/com/albedo/java/common/config/JacksonHttpMessageConvertersConfiguration.java
@@ -0,0 +1,33 @@
+package com.albedo.java.common.config;
+
+import com.albedo.java.common.config.formart.CustomDateFormt;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.text.DateFormat;
+
+@Configuration
+public class JacksonHttpMessageConvertersConfiguration {
+
+    @Autowired
+    private Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
+
+    @Bean
+    public MappingJackson2HttpMessageConverter MappingJsonpHttpMessageConverter() {
+
+        ObjectMapper mapper = jackson2ObjectMapperBuilder.build();
+
+        // ObjectMapper为了保障线程安全性，里面的配置类都是一个不可变的对象
+        // 所以这里的setDateFormat的内部原理其实是创建了一个新的配置类
+        DateFormat dateFormat = mapper.getDateFormat();
+        mapper.setDateFormat(new CustomDateFormt(dateFormat));
+
+        MappingJackson2HttpMessageConverter mappingJsonpHttpMessageConverter = new MappingJackson2HttpMessageConverter(mapper);
+        return mappingJsonpHttpMessageConverter;
+    }
+
+}
---
+++ albedo-boot-api/albedo-boot-service/src/main/java/com/albedo/java/common/config/formart/CustomDateFormt.java
@@ -0,0 +1,64 @@
+package com.albedo.java.common.config.formart;
+
+import com.albedo.java.util.DateUtil;
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Date;
+
+public class CustomDateFormt extends DateFormat {
+
+    private DateFormat dateFormat;
+
+
+    public CustomDateFormt(DateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+    @Override
+    public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
+        return dateFormat.format(date, toAppendTo, fieldPosition);
+    }
+
+    @Override
+    public Date parse(String source, ParsePosition pos) {
+
+        Date date;
+
+        try {
+            date = DateUtil.parseDate(source);
+        } catch (Exception e) {
+
+            date = dateFormat.parse(source, pos);
+        }
+
+        return date;
+    }
+
+    // 主要还是装饰这个方法
+    @Override
+    public Date parse(String source) throws ParseException {
+
+        Date date;
+
+        try {
+            // 先按我的规则来
+            date = DateUtil.parseDate(source);
+        } catch (Exception e) {
+            // 不行，那就按原先的规则吧
+            date = dateFormat.parse(source);
+        }
+
+        return date;
+    }
+
+    // 这里装饰clone方法的原因是因为clone方法在jackson中也有用到
+    @Override
+    public Object clone() {
+        Object format = dateFormat.clone();
+        return new CustomDateFormt((DateFormat) format);
+    }
+
+}
